cf0051543e330ca4e230aca5b0fc4da265c628bb
keunlas <keunlaz@gmail.com>
2023-03-17 18:54:01 +0800

    ${mytime} : upload bakup by script

--- push_to_github.sh
+++ push_to_github.sh
@@ -8,6 +8,8 @@ cd /home/ubuntu/Python-Notebook/
 #echo '==============================' >> ./push_to_github.log
 #git push >> ./push_to_github.log
 
+mytime=`date`
+
 git add .
-git commit -m 'upload bakup by script'
+git commit -m '${mytime} : upload bakup by script'
 git push > ./push_to_github.log
