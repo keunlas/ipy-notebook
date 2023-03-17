f9e538572d63c8f220d9bf20a9fab01fd1f45c17
keunlas <keunlaz@gmail.com>
2023-03-17 18:49:01 +0800

    upload bakup by script

--- push_to_github.sh
+++ push_to_github.sh
@@ -1,9 +1,13 @@
 cd /home/ubuntu/Python-Notebook/
 
-echo '' > ./push_to_github.log
+#echo '' > ./push_to_github.log
 
-git add . >> ./push_to_github.log
-echo '==============================' >> ./push_to_github.log
-git commit -m 'upload bakup by script' >> ./push_to_github.log
-echo '==============================' >> ./push_to_github.log
-git push >> ./push_to_github.log
+#git add . >> ./push_to_github.log
+#echo '==============================' >> ./push_to_github.log
+#git commit -m 'upload bakup by script' >> ./push_to_github.log
+#echo '==============================' >> ./push_to_github.log
+#git push >> ./push_to_github.log
+
+git add .
+git commit -m 'upload bakup by script'
+git push > ./push_to_github.log
